b8cfe4fa067d130065f97321adb3d2f537eeb1b6
Matt Glover <matthewglover@me.com>
2017-07-24 17:02:02 +0100

    Adds Hello World Java FX example
    Based on code at http://docs.oracle.com/javafx/2/get_started/hello_world.htm

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        Button button = new Button();
+        button.setText("Say 'Hello world!'");
+        button.setOnAction(event -> System.out.println("Hello world!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
